d4f74d0f6ef57ad141cc0ddcc66748fcd240084e
Nathan Frey <nfrey213@gmail.com>
2021-02-26 11:27:16 -0500

    unsupervised segmentation

--- aai/segment/image_segment.py
+++ aai/segment/image_segment.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 """
 Version 4/14/2020
 
@@ -19,10 +18,11 @@ import os
 import sys
 import re
 import timeit
+
+import torch
+
 import skimage.filters
 import skimage.measure
-
-
 from skimage.transform import resize
 from skimage.filters import threshold_otsu, threshold_local, rank
 from skimage.segmentation import clear_border, watershed
@@ -34,13 +34,47 @@ from PIL import Image as pimage
 from PIL import ImageFile
 from io import BytesIO
 
+from aai.segment.autoencoder import AutoencoderBottle
+
 
 def uniq(input):
-	output = []
-	for x in input:
-		if x not in output:
-			output.append(x)
-	return output
+    output = []
+    for x in input:
+        if x not in output:
+            output.append(x)
+    return output
+
+
+def model_segment(image, model_name, model_file):
+    """Segment image with a trained model.
+
+    Arguments
+    ---------
+    image: np.array
+    model_name: str
+        Model to instantiate.
+    model_file: str
+        Filename of trained model
+
+    Returns
+    -------
+    contour: np.array
+        Detected image contours
+
+    """
+
+    if model_name == "AutoencoderBottle":
+        model = AutoencoderBottle()
+
+    model.load_state_dict(torch.load(model_file))
+    model.eval()
+    tensor_im = torch.Tensor(image)
+    contour = model(tensor_im)
+
+    # TODO: not sure if need optimizer and loss function here too
+
+    return contour
+
 
 def binary_segment(image, sigma=0.1, image_type='dark_field', algo='bimodal'):
     """
@@ -49,38 +83,42 @@ def binary_segment(image, sigma=0.1, image_type='dark_field', algo='bimodal'):
     """
     # image = np.asarray(pimage.open(ff).convert('L')) / 255
 
-    image = skimage.filters.gaussian(image, sigma=sigma) #, multichannel=False, preserve_range=True)
+    image = skimage.filters.gaussian(
+        image, sigma=sigma)  #, multichannel=False, preserve_range=True)
 
     if algo == 'bimodal':
-    # apply threshold
+        # apply threshold
         thresh = threshold_otsu(image)
         # print('thresh bimodal')
         print('thresh otsu: %f' % thresh)
 
     elif algo == 'local':
-        bsize = 35 # int(np.amin(image.shape) / 2) + 1
+        bsize = 35  # int(np.amin(image.shape) / 2) + 1
         thresh = threshold_local(image, bsize)
         print('thresh local: %f' % thresh)
 
-   # remove artifacts connected to image border
+
+# remove artifacts connected to image border
     if image_type == 'bright_field':
         bgl = 1
     elif image_type == 'dark_field':
         bgl = 0
     else:
         sys.exit()
-        
+
     bw = closing(image < thresh)
     cleared = clear_border(bw)
     label_image = label(cleared)
-#     label_image = label(bw)
+    #     label_image = label(bw)
 
-    image_label_overlay = label2rgb(label_image, image=image, bg_label = 0)
-    
+    image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
+
     regionlist = regionprops(label_image)
-    regionlist = sorted(regionlist, key=lambda x: (x.centroid[0],(x.centroid[1])))
+    regionlist = sorted(
+        regionlist, key=lambda x: (x.centroid[0], (x.centroid[1])))
 
     return regionlist, label_image, image_label_overlay
+
 
 def watershed_segment(image, thresh):
 
@@ -101,12 +139,11 @@ def watershed_segment(image, thresh):
     # process the watershed
     label_image = watershed(gradient, markers)
 
-    image_label_overlay = label2rgb(label_image, image=image, bg_label = 0)
-    
+    image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
+
     regionlist = regionprops(label_image)
-    regionlist = sorted(regionlist, key=lambda x: (x.centroid[0],(x.centroid[1])))
-
-
+    regionlist = sorted(
+        regionlist, key=lambda x: (x.centroid[0], (x.centroid[1])))
 
     return regionlist, label_image, image_label_overlay
 
@@ -115,22 +152,21 @@ def segment_main(image_data):
     pass
     return
 
+
 if __name__ == '__main__':
 
-	print(pd.__version__)
-	print(skimage.__version__)
-	pd.set_option('display.expand_frame_repr', False, 'display.max_columns', None)
-	cwd = os.getcwd()
+    print(pd.__version__)
+    print(skimage.__version__)
+    pd.set_option('display.expand_frame_repr', False, 'display.max_columns',
+                  None)
+    cwd = os.getcwd()
 
-	if 'Chris Price' in cwd:
-		datadir = 'C:\\Users\\Chris Price\\Box Sync\\projects\\'
-	else:
-		print('add your path here')
-		sys.exit()
+    if 'Chris Price' in cwd:
+        datadir = 'C:\\Users\\Chris Price\\Box Sync\\projects\\'
+    else:
+        print('add your path here')
+        sys.exit()
 
-	os.chdir(datadir +'11282018_Au_SA_withlight-as-assem\\')
+    os.chdir(datadir + '11282018_Au_SA_withlight-as-assem\\')
 
-	files = os.listdir()
-
-
-
+    files = os.listdir()
---
+++ aai/segment/utils.py
@@ -0,0 +1,85 @@
+"""Utils for image data processing and augmentation."""
+import numpy as np
+import matplotlib.pyplot as plt
+import skimage
+from skimage import filters, transform, util
+import os
+
+
+def fourfold_rotate(image):
+    """Rotate image by 90, 180, and 270 degrees.
+
+    Arguments
+    ---------
+    image: np.array
+
+    Returns
+    -------
+    rotated_images: List
+
+    """
+
+    rotations = [90, 180, 270]
+    rotated_images = [transform.rotate(image, r) for r in rotations]
+    return rotated_images
+
+
+def crop_image(image, step_size: int = 512):
+    """
+    Chop image into grids to increase dataset size and reduce memory
+    requirements.
+
+    Arguments
+    ---------
+    image: np.array
+    step_size: int
+        Images will be cropped into tiles of dimension 
+        (step_size, step_size).
+
+    Returns
+    -------
+    cropped_images: np.array
+
+    """
+
+    windows = util.view_as_windows(
+        image, window_shape=(step_size, step_size), step=step_size)
+    cropped_images = windows.reshape(-1, step_size, step_size)
+
+    return cropped_images
+
+
+def normalize_im(image):
+    """Scale and normalize pixel values to be between 0 and 1.
+
+    Arguments
+    ---------
+    image: np.array
+
+    Returns
+    -------
+    normed: np.array
+
+    """
+
+    scaled = (image - np.mean(image) / np.std(image))
+    normed = (scaled - np.min(scaled)) / (np.max(scaled) - np.min(scaled))
+
+    return normed
+
+
+def blur_im(image, sigma: int = 3):
+    """Apply Gaussian filter to reduce image noise.
+
+    Arguments
+    ---------
+    image: np.array
+    sigma: int
+
+    Returns
+    -------
+    blurred: np.array
+
+    """
+
+    return filters.gaussian(image, sigma)
